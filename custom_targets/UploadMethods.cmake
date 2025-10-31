fecea2ae72b05b0a38b5a963b4fc2459e6d7d05b
Jack N <nystromjp@gmail.com>
2025-10-30 17:07:52 -0700

    stm32-test: update openocd config to use stlink-dap

--- custom_targets/UploadMethods.cmake
+++ custom_targets/UploadMethods.cmake
@@ -7,7 +7,8 @@ set(UPLOAD_METHOD_DEFAULT OPENOCD)
 
 set(OPENOCD_UPLOAD_ENABLED true)
 set(OPENOCD_CHIP_CONFIG_COMMANDS
-    -f ${OpenOCD_SCRIPT_DIR}/interface/stlink.cfg
-    -f ${OpenOCD_SCRIPT_DIR}/target/stm32g4x.cfg
+    -f interface/stlink-dap.cfg
+    -c "transport select dapdirect_swd"
+    -f target/stm32g4x.cfg
 )
 
