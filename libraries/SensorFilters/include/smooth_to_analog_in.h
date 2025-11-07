c9727b2a532b95521b5e05264fabc53afd72e6a9
Jackson Pinsonneault <jpins@college10-169-233-118-168.resnet.ucsc.edu>
2025-11-06 22:00:23 -0800

    Changed SmoothToAnalogIn to use exponential moving average rather than rolling average

--- libraries/SensorFilters/include/smooth_to_analog_in.h
+++ libraries/SensorFilters/include/smooth_to_analog_in.h
@@ -11,39 +11,35 @@
 class SmoothToAnalogIn {
 
 public:
-    /** Create a smoothed AnalogIn using rolling-average
+    /** Create a smoothed AnalogIn using EMA (exponential moving average)
      *
      * @param analog_pin Reference to an AnalogIn to use for reading
-     * @param window_size (optional) Rolling-average window size (defaults to 3)
-     * @param force_repopulate_time (optional) Time (in milliseconds) where a repopulation of the rolling-average must occur if the last read was greater than this time (defaults to 100ms)
-     *
-     * @note If force_repopulate_time has passed since the last read, then a repopulation of the rolling-average window will occur with 1ms delays for between each reread
+     * @param alpha (optional) Smoothing factor used for EMA (larger the value, the less impact recent values hold)
+     * @param force_reset_time (optional) Time (in milliseconds) where the EMA resets if the last read's difference in time was greater than this (defaults to 100ms)
     */
-    explicit SmoothToAnalogIn(AnalogIn &analog_pin, const uint16_t window_size=3, const uint16_t force_repopulate_time=100) :
+    explicit SmoothToAnalogIn(AnalogIn &analog_pin, const float alpha=0.7, const uint16_t force_reset_time=100) :
         _analog_pin(analog_pin),
-        _force_repopulate_time(force_repopulate_time),
-        _window_size(window_size) {
-        _held_values = new float[_window_size];
-        _timer.start();
-    }
+        _force_reset_time(force_reset_time),
+        _alpha(alpha)
+        { _timer.start(); }
 
-    /** Read the referenced AnalogIn input voltage as a rolling-average value, represented as a float in the range [0.0, 1.0]
+    /** Read the referenced AnalogIn input voltage as an EMA value, represented as a float in the range [0.0, 1.0]
      *
-     * @returns A floating-point value representing the rolling-average of the last window_size reads, measured as a percentage
+     * @returns A floating-point value representing the EMA read, measured as a percentage
      *
-     * @note If force_repopulate_time has passed since the last read, then a repopulation of the rolling-average window will occur with 1ms delays for between each reread
+     * @note If force_repopulate_time has passed since the last read, then the EMA is reset
     */
     float read();
 
-    /** Read the referenced AnalogIn input voltage as a rolling-average value, represented as an unsigned short in the range [0x0, 0xFFFF]
+    /** Read the referenced AnalogIn input voltage as an EMA value, represented as an unsigned short in the range [0x0, 0xFFFF]
      *
-     * @returns 16-bit unsigned short representing the rolling-average of the last window_size reads, normalized to a 16-bit value
+     * @returns 16-bit unsigned short representing the EMA read, normalized to a 16-bit value
      *
-     * @note If force_repopulate_time has passed since the last read, then a repopulation of the rolling-average window will occur with 1ms delays for between each reread
+     * @note If force_repopulate_time has passed since the last read, then the EMA is reset
     */
     unsigned short read_u16();
 
-    /** Read the referenced AnalogIn input voltage as a rolling-average in volts. The output depends on the target board's
+    /** Read the referenced AnalogIn input voltage as an EMA in volts. The output depends on the target board's
      * ADC reference voltage (typically equal to supply voltage). The ADC reference voltage
      * sets the maximum voltage the ADC can quantify (ie: ADC output == ADC_MAX_VALUE when Vin == Vref)
      *
@@ -51,9 +47,9 @@ public:
      * option target.default-adc_vref. The reference voltage for a particular input
      * can be manually specified by either the constructor or `AnalogIn::set_reference_voltage`.
      *
-     * @returns A floating-point value representing the rolling-average of the last window_size reads, measured in volts.
+     * @returns A floating-point value representing the EMA read, measured in volts.
      *
-     * @note If force_repopulate_time has passed since the last read, then a repopulation of the rolling-average window will occur with 1ms delays for between each reread
+     * @note If force_repopulate_time has passed since the last read, then the EMA is reset
     */
     float read_voltage();
 
@@ -63,7 +59,7 @@ public:
      *
      * @param[in] vref New ADC reference voltage for the referenced AnalogIn.
     */
-    void set_reference_voltage(float vref);
+    void set_reference_voltage(float vref) const;
 
     /** Gets the referenced AnalogIn's reference voltage.
      *
@@ -71,20 +67,27 @@ public:
     */
     float get_reference_voltage() const;
 
-private:
-    AnalogIn &_analog_pin;              //  Referenced AnalogIn which is used for reads
-    uint16_t _force_repopulate_time;    //  If this much time (in ms) passes since the last read, then run populate procedure
-    uint16_t _window_size;              //  Rolling-average window size
-    float* _held_values = nullptr;      //  Rolling-average previously read values
-    float _held_values_summed = 0;      //  Sum of the last rolling-average window values
-    bool _prepopulated = false;         //  States whether the rolling-average window has been filled
-    Timer _timer;                       //  Detects when a forced repopulate should occur
-
-    /** Updates the current rolling-average window to have sample as its newest value
+    /** Sets how much recent reads affect the returned value of read()
      *
-     * @param sample A floating-point value representing the current input voltage, measured as a percentage
+     * @param alpha Smoothing factor used for EMA (larger the value, the less impact recent values hold)
+     *
+     * @note 0 < alpha < 1 for proper EMA smoothing
     */
-    void _add_sample(float sample);
+    void set_alpha(float alpha);
+
+private:
+    AnalogIn &_analog_pin;          //  Referenced AnalogIn which is used for reads
+    uint16_t _force_reset_time;     //  If this much time (in ms) passes since the last read, then reset the EMA
+    float _alpha;                   //  The EMA smoothing factor
+    float _smoothed_value = 0;      //  The current value that should be returned by read()
+    bool _is_initialized = false;   //  States whether the EMA has started
+    Timer _timer;                   //  Detects when a forced reset should occur
+
+    /** Calculates the new _smoothed_value according to the _alpha level and the current value
+     *
+     * @param raw_value A floating-point value representing the current read as a percentage
+    */
+    float _compute_EMA(float raw_value);
 };
 
 #endif //MBED_OS_SMOOTHTOANALOGIN_H
--- libraries/SensorFilters/smooth_to_analog_in.cpp
+++ libraries/SensorFilters/smooth_to_analog_in.cpp
@@ -5,17 +5,15 @@
 #include "smooth_to_analog_in.h"
 
 float SmoothToAnalogIn::read() {
-    float read_output = _analog_pin.read();
     const auto time_dif = chrono::duration_cast<std::chrono::milliseconds>(_timer.elapsed_time());
     _timer.reset();
 
-    if (time_dif >= static_cast<std::chrono::milliseconds>(_force_repopulate_time)) {
-        _prepopulated = false;
+    if (time_dif >= static_cast<std::chrono::milliseconds>(_force_reset_time)) {
+        _is_initialized = false;
     }
 
-    _add_sample(read_output);
-
-    return _held_values_summed / static_cast<float>(_window_size);
+    const float raw_value = _analog_pin.read();
+    return SmoothToAnalogIn::_compute_EMA(raw_value);
 }
 
 unsigned short SmoothToAnalogIn::read_u16() {
@@ -26,7 +24,7 @@ float SmoothToAnalogIn::read_voltage() {
     return read() * _analog_pin.get_reference_voltage();
 }
 
-void SmoothToAnalogIn::set_reference_voltage(float vref) {
+void SmoothToAnalogIn::set_reference_voltage(float vref) const {
     _analog_pin.set_reference_voltage(vref);
 }
 
@@ -34,31 +32,17 @@ float SmoothToAnalogIn::get_reference_voltage() const {
     return _analog_pin.get_reference_voltage();
 }
 
+void SmoothToAnalogIn::set_alpha(const float alpha) {
+    _alpha = alpha;
+}
 
-void SmoothToAnalogIn::_add_sample(float sample) {
-    if (!_prepopulated) {
-        _held_values_summed = sample;
-        _held_values[_window_size - 1] = sample;
-        float read_output;
+float SmoothToAnalogIn::_compute_EMA(const float raw_value) {
 
-        for (int i = _window_size - 2; i >= 0; i--) {
-            ThisThread::sleep_for(1ms);
-
-            read_output = _analog_pin.read();
-            _held_values_summed += read_output;
-            _held_values[i] = read_output;
-        }
-
-        _prepopulated = true;
-        return;
+    if (!_is_initialized) {
+        _smoothed_value = raw_value;
+        _is_initialized = true;
+    } else {
+        _smoothed_value = (_alpha * raw_value) + ((1.0f - _alpha) * _smoothed_value);
     }
-
-    _held_values_summed -= _held_values[_window_size - 1];
-    _held_values_summed += sample;
-
-    for (int i = _window_size - 2; i >= 0; i--) {
-        _held_values[i+1] = _held_values[i];
-    }
-
-    _held_values[0] = sample;
+    return _smoothed_value;
 }
