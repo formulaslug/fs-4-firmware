f4a8079144a425d2b6ab16c5e91c389790ddd41d
Daniel Du <ydu66@ucsc.edu>
2025-12-13 00:05:27 -0800

    updated / added openocd configs for all boards
    still gets a open failed error

--- custom_targets/UploadMethods.cmake
+++ custom_targets/UploadMethods.cmake
@@ -6,9 +6,28 @@ set(UPLOAD_METHOD_DEFAULT OPENOCD)
 # OpenOCD configs
 
 set(OPENOCD_UPLOAD_ENABLED true)
-set(OPENOCD_CHIP_CONFIG_COMMANDS
-    -f interface/stlink-dap.cfg
-    -c "transport select dapdirect_swd"
-    -f target/stm32g4x.cfg
-)
 
+#stm32g4x boards
+if (MBED_TARGET STREQUAL "PERIPHERAL_BOARD" OR
+    MBED_TARGET STREQUAL "STEERING_DASH_BOARD" OR
+    MBED_TARGET STREQUAL "STM32_TEST_BOARD_V1" OR
+    MBED_TARGET STREQUAL "TRACTIVE_BATTERY_BOARD")
+
+    set(OPENOCD_CHIP_CONFIG_COMMANDS
+        -f interface/stlink.cfg
+        -c "transport select hla_swd"
+        -f target/stm32g4x.cfg
+    )
+
+endif()
+
+#stm32f4x boards
+if (MBED_TARGET STREQUAL "STEERING_DASH_BOARD_F4")
+
+    set(OPENOCD_CHIP_CONFIG_COMMANDS
+        -f interface/stlink.cfg
+        -c "transport select hla_swd"
+        -f target/stm32f4x.cfg
+    )
+
+endif()
