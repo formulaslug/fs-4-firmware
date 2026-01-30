d55c8ba6738dcda5e53ba9658fc094003ab8dea7
Daniel Du <ydu66@ucsc.edu>
2026-01-30 15:35:29 -0800

    Update UploadMethods.cmake, should now work for VCU and Tractive Battery boards

--- custom_targets/UploadMethods.cmake
+++ custom_targets/UploadMethods.cmake
@@ -1,11 +1,23 @@
 # set(MBED_UPLOAD_ENABLED true)
-# set(MBED_RESET_BAUDRATE 115200) #?
+# set(MBED_RESET_BAUDRATE 115200)
 
+
+# OPENOCD Upload Method
 set(UPLOAD_METHOD_DEFAULT OPENOCD)
-
-# OpenOCD configs
-
 set(OPENOCD_UPLOAD_ENABLED true)
+
+
+# STLINK Upload Method, here in case OPENOCD doesn't work
+#set(UPLOAD_METHOD_DEFAULT = STLINK)
+#set(STLINK_UPLOAD_ENABLED true)
+#set(STLINK_ARGS --version) # send commands to stlink, no need to uncomment
+
+
+# STM32CUBE Upload Method, here in case STLINK doesn't work
+#set(UPLOAD_METHOD_DEFAULT = STM32CUBE)
+#set(STM32CUBE_UPLOAD_ENABLED true)
+#set(STM32CUBE_CONNECT_COMMAND port=SWD)
+#set(STM32CubeProg_PATH "C:\\Program Files\\STMicroelectronics\\STM32Cube\\STM32CubeProgrammer\\bin\\STM32_Programmer_CLI.exe")
 
 #stm32g4x boards
 if (MBED_TARGET STREQUAL "PERIPHERAL_BOARD" OR
@@ -17,17 +29,19 @@ if (MBED_TARGET STREQUAL "PERIPHERAL_BOARD" OR
         -f interface/stlink.cfg
         -c "transport select hla_swd"
         -f target/stm32g4x.cfg
-    )
+        )   # comment this out if not using OPENOCD
 
 endif()
 
 #stm32f4x boards
-if (MBED_TARGET STREQUAL "STEERING_DASH_BOARD_F4")
+if (MBED_TARGET STREQUAL "STEERING_DASH_BOARD_F4" OR
+    MBED_TARGET STREQUAL "TRACTIVE_BATTERY_BOARD_F4" OR
+    MBED_TARGET STREQUAL "VEHICLE_CONTROL_BOARD_F4")
 
     set(OPENOCD_CHIP_CONFIG_COMMANDS
         -f interface/stlink.cfg
         -c "transport select hla_swd"
         -f target/stm32f4x.cfg
-    )
+        )   # comment this out if not using OPENOCD
 
 endif()
