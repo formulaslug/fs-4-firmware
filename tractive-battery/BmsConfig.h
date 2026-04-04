5669acc1e2eac41d84d9d24eedecdeda466a2ac7
Ethan Moore <ethanjamesmoore2004@gmail.com>
2026-04-04 16:11:59 -0700

    fixed spi init

--- tractive-battery/BmsConfig.h
+++ tractive-battery/BmsConfig.h
@@ -153,7 +153,7 @@ class BMS{
       BufferedSerial VCP_UART = BufferedSerial(PA_9, PA_10); // some configuration for this needs to be done at startup see mbedosce
       CAN CAN_POWERTRAIN = CAN(PA_11, PA_12);
       DigitalOut nPrechargeControl = DigitalOut(PB_0,1); // no precharge at this point
-      SPI spiInterface = SPI(PB_4, PB_5, PB_10, PB_9, use_gpio_ssel);
+      SPI spiInterface = SPI(PB_5, PB_4, PB_10, PB_9, use_gpio_ssel);
       DigitalOut TS1W_PU_Control = DigitalOut(PB_15);
       OneWire TS1W = OneWire(PB_14); // look up more on 1 wire interface 
 
