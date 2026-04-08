3a7eee2e3e45f8c58884abc72e9eff35f223b64b
Jackson Pinsonneault <jpins@eduroam-169-233-144-130.ucsc.edu>
2026-04-07 18:00:27 -0700

    Unfinished updating of vcu refactor

--- vcu/etc/etc_controller.h
+++ vcu/etc/etc_controller.h
@@ -1,103 +1,106 @@
+//
+// Created by Jackson Pinsonneault on 3/24/26.
+//
+
 #ifndef ETC_CONTROLLER_H
 #define ETC_CONTROLLER_H
 
-#include "AnalogIn.h"
-#include "DigitalIn.h"
-#include "DigitalOut.h"
-#include "PinNames.h"
-#include "Timer.h"
 #include "mbed.h"
-#include <cstdint>
 
 struct ETCState {
     float APPS1_voltage = 0.0f;
     float APPS2_voltage = 0.0f;
-    float APPS1_position = 0.0f;
-    float APPS2_position = 0.0f;
+    float APPS1_position = 0.0f; // 0 to 1
+    float APPS2_position = 0.0f; // 0 to 1
     float APPS_position_avg = 0.0f;
     float BPPS_voltage = 0.0f;
-    float BPPS_position = 0.0f;
+    float BPPS_position = 0.0f; // 0 to 1
     float front_BSE_voltage = 0.0f;
     float rear_BSE_voltage = 0.0f;
-    bool RTD_button_active = false;
-    bool RTD_state = false;
+    bool rtd_button_pressed = false;
+    bool ready_to_drive = false;
     bool motor_enabled = false;
-    bool APPS_deviation_implaus = false;
-    bool APPS_range_implaus = false;
-    bool BPPS_range_implaus = false;
-    bool brake_and_accel_implaus = false;
-    bool TS_active = false;
+    bool implaus_APPS_deviation = false;
+    bool implaus_APPS_range = false;
+    bool implaus_BPPS_range = false;
+    bool implaus_BSE_range = false;
+    bool implaus_brake_and_accel = false;
     bool can_regen = false;
     bool must_use_hydraulic_brakes = false;
+    bool GLV_ok = false;
+    bool shutdown_closed = false;
 };
 
 class ETCController {
 public:
-    ETCController(PinName APPS1_pin, PinName APPS2_pin, PinName BPPS_pin, PinName front_BSE_pin, PinName rear_BSE_pin, PinName RTD_button_pin, PinName RTD_light_pin, PinName RTD_buzzer_pin, PinName BSPD_fault_pin);
+    ETCState state;
+
+    ETCController(PinName APPS1_pin, PinName APPS2_pin, PinName BPPS_pin, PinName front_BSE_pin, PinName rear_BSE_pin, PinName rtd_button_pin, PinName rtd_light_pin, PinName rtd_buzzer_pin, PinName BSPD_fault_pin);
 
     bool update_state();
-    
-    static float clamp(float value);
-
-    static bool in_range(float value, float low, float high, float margin);
-
-    void update_implaus();
 
     void update_RTD(bool GLV_charged);
 
     void update_regen(float speed);
-
-    void update_implaus_timer(Timer &timer, bool &timer_running, float min_time, bool implaus_state, bool &etc_implaus); 
-
-    void toggle_RTD(bool new_RTD_state);
-
-    ETCState get_ETC_state();
 private:
-    ETCState ETC_state;
-
     AnalogIn APPS1_input;
     AnalogIn APPS2_input;
     AnalogIn BPPS_input;
     AnalogIn front_BSE_input;
     AnalogIn rear_BSE_input;
 
-    DigitalIn RTD_button;
+    DigitalIn rtd_button;
     DigitalIn BSPD_fault_input;
 
-    DigitalOut RTD_light;
-    DigitalOut RTD_buzzer;
+    DigitalOut rtd_light;
+    DigitalOut rtd_buzzer;
 
-    Timeout RTD_buzzer_timeout;
+    Timeout rtd_buzzer_timeout;
 
-    static constexpr std::chrono::seconds RTD_buzzer_duration = 2s;
+    static constexpr std::chrono::seconds RTD_BUZZER_DURATION = 2s;
 
-    static constexpr float APPS1_min_voltage = 0.3125f;
-    static constexpr float APPS1_max_voltage = 2.8125f;
+    static constexpr float APPS1_MIN_VOLTAGE = 0.3125f;
+    static constexpr float APPS1_MAX_VOLTAGE = 2.8125f;
 
-    static constexpr float APPS2_min_voltage = 0.1875f;
-    static constexpr float APPS2_max_voltage = 1.6875f;
+    static constexpr float APPS2_MIN_VOLTAGE = 0.1875f;
+    static constexpr float APPS2_MAX_VOLTAGE = 1.6875f;
 
-    static constexpr float BPPS_min_voltage = 0.3125f;
-    static constexpr float BPPS_max_voltage = 2.8125f;
+    static constexpr float BPPS_MIN_VOLTAGE = 0.3125f;
+    static constexpr float BPPS_MAX_VOLTAGE = 2.8125f;
 
-    static constexpr float PS_voltage_margin = 0.05f;
+    static constexpr float FRONT_BSE_MIN_VOLTAGE = 0.3125f;
+    static constexpr float FRONT_BSE_MAX_VOLTAGE = 2.8125f;
 
-    static constexpr float front_BSE_activation_voltage = 0.5f;
-    static constexpr float rear_BSE_activation_voltage = 0.5f;
-    static constexpr float BPPS_max_non_regen_braking = 0.9f;
+    static constexpr float REAR_BSE_MIN_VOLTAGE = 0.3125f;
+    static constexpr float REAR_BSE_MAX_VOLTAGE = 2.8125f;
+
+    static constexpr float FRONT_BSE_ACTIVATION_VOLTAGE = 0.5f;
+    static constexpr float REAR_BSE_ACTIVATION_VOLTAGE = 0.5f;
+    static constexpr float BPPS_MAX_NON_REGEN_BRAKING = 0.9f;
     
-    static constexpr float BPPS_brake_engage_percent = 0.1f;
-    static constexpr float max_APPS_position_deviation = 0.10f;
-    static constexpr float implaus_min_time = 0.100f; // when greater than 0.1s, activate implaus
+    static constexpr float BPPS_BRAKE_ENGAGE_PERCENT = 0.1f;
+    static constexpr float MAX_APPS_POSITION_DEVIATION = 0.10f;
 
-    Timer APPS_deviation_implaus_timer;
-    Timer APPS_range_implaus_timer;
-    Timer BPPS_range_implaus_timer;
-    Timer brake_and_accel_implaus_timer;
-    bool APPS_deviation_implaus_timer_runnning = false;
-    bool APPS_range_implaus_timer_running = false;
-    bool BPPS_range_implaus_timer_running = false;
-    bool brake_and_accel_implaus_timer_running = false;
+    Timer implaus_APPS_deviation_timer;
+    Timer implaus_APPS_range_timer;
+    Timer implaus_BPPS_range_timer;
+    Timer implaus_BSE_range_timer;
+    Timer implaus_brake_and_accel_timer;
+    bool implaus_APPS_deviation_timer_running = false;
+    bool implaus_APPS_range_timer_running = false;
+    bool implaus_BPPS_range_timer_running = false;
+    bool implaus_BSE_range_timer_running = false;
+    bool implaus_brake_and_accel_timer_running = false;
+
+    static float clamp(float value);
+
+    static bool in_range(float value, float low, float high);
+
+    void update_implaus();
+
+    void update_implaus_timer(Timer &timer, bool &timer_running, bool implaus_state, bool &etc_implaus); 
+
+    void enable_RTD();
 };
 
 #endif
