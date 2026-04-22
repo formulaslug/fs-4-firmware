f0aa1ba0067829c890c606fe6c8251b37941aeb3
Ethan Moore <ethanjamesmoore2004@gmail.com>
2026-04-21 20:55:47 -0700

    update can.cpp

--- tractive-battery/Can.cpp
+++ tractive-battery/Can.cpp
@@ -77,7 +77,7 @@ CANMessage ACC_TPDO_SEG4_TEMPS(int8_t *temps) {
 CANMessage ACC_TPDO_SEG0_VOLTS(uint16_t *volts) {
     uint8_t data[NUM_VOLTAGES_PER_MODULE];
     for (int i = 0; i < NUM_VOLTAGES_PER_MODULE; i++) {
-        data[i] = (uint8_t)(BMSInstance.voltages[0][i] / 10 - 200);
+        data[i] = (uint8_t)(BMSInstace.voltages[0][i] / 10 - 200);
     }
 
     return CANMessage{kACC_TPDO_SEG0_VOLTS, data, NUM_VOLTAGES_PER_MODULE};
