7d3449c237a41cb95821fef7bfa16fe4cfb65476
MaiJonathan <jonathanbmai@gmail.com>
2026-04-30 16:55:56 -0700

    Initial Code for steering-wheel\main

---
+++ steering-wheel/pin_config.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "mbed.h"
+
+//Pin Stuff
+constexpr PinName PIN_MOSI = PA_7;
+constexpr PinName PIN_MISO = PA_6;
+constexpr PinName PIN_SCK = PA_5;
+constexpr PinName PIN_CS = PA_4;
+constexpr PinName PIN_PDN = PA_15;
+constexpr PinName PIN_IRQ = PB_7;
+constexpr PinName PIN_LAYOUT = PB_4;
+constexpr PinName PIN_TC = PB_0; //ADC1_IN15, Got these values from custom_targets folder
+constexpr PinName PIN_REGEN = PA_0; //ADC1_IN1
+constexpr PinName PIN_MODE = PA_1; //ADC1_IN2
+constexpr PinName PIN_CAN_RX = PA_11; //FDCAN1_RX
+constexpr PinName PIN_CAN_TX = PA_12; //FDCAN1_TX
+
+constexpr uint32_t CAN_FREQUENCY = 500000;
