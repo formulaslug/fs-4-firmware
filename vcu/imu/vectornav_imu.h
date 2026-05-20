637c3002d5b6315c800e76fbd82a0747e1e42047
Rishabh <61441648+rishabhroyy@users.noreply.github.com>
2026-05-20 13:18:04 -0700

    consolidate vectornav functions

--- vcu/imu/vectornav_imu.h
+++ vcu/imu/vectornav_imu.h
@@ -15,6 +15,15 @@ inline void check_vn_error(const char* file, int line, VN::Error err) {
 
 #define CHECK_VN_ERR(err) check_vn_error(__FILE__, __LINE__, err)
 
+struct VectornavState {
+    VN::Vec3f accel{0,0,0};
+    VN::Vec3f angRate{0,0,0};
+    VN::Vec3f mag{0,0,0};
+    VN::Ypr ypr{0,0,0};
+    VN::Lla pos{0,0,0};
+    VN::Vec3f velBody{0,0,0};
+};
+
 class VectorNavIMU {
     public:
     /** Creates a VectorNavIMU
@@ -27,6 +36,7 @@ class VectorNavIMU {
     /** Connects to the IMU
      *
      * @returns VN::Error::None if succeeded, otherwise one of the errors under VN::Error
+     * Important: spawns a thread
     */
     VN::Error connect();
 
@@ -35,108 +45,16 @@ class VectorNavIMU {
     */
     void disconnect();
 
-    /** Sensor initialization sequence:
-     * 1. Call initRegisters() before enabling any functions
-     * 2. Enable selected functions by calling their enable method
-     * 3. Call applyRegisters() to send config to the IMU
-    */
-
-    /** Initializes Default Register Values
-     *
-    */
-    void initRegisters();
-
     /** Enables raw IMU functionality (accel, angular rate, magnetometer, temp)
-     *
-     * @param enabled Whether to enable the function
-     * @param rateDivisor The divisor for sampling rate. i.e. if sensor running at 800MHz, and rateDivisor is 8, 800/1=800MHz
-    */
-    void enableRawImu(bool enabled, uint16_t rateDivisor = 1);
-
-    /** Enables attitude (YPR, Quaternion)
-     *
-     * @param enabled Whether to enable the function
-     * @param rateDivisor The divisor for sampling rate. i.e. if sensor running at 800MHz, and rateDivisor is 8, 800/8=100MHz
-    */
-    void enableAttitude(bool enabled, uint16_t rateDivisor = 8);
-
-    /** Enables navigation (position, velocity body, velocity ned)
-     *
-     * @param enabled Whether to enable the function
-     * @param rateDivisor The divisor for sampling rate. i.e. if sensor running at 800MHz, and rateDivisor is 8, 800/8=100MHz
-    */
-    void enableNavigation(bool enabled, uint16_t rateDivisor = 8);
-
-    /** Enables GPS time (GPS time, UTC, Week, Time of Week)
-     *
-     * @param enabled Whether to enable the function
-     * @param rateDivisor The divisor for sampling rate. i.e. if sensor running at 800MHz, and rateDivisor is 80, 800/80=10MHz
-    */
-    void enableGpsTime(bool enabled, uint16_t rateDivisor = 80);
-
-    /** Enables uncertainty (position uncertainty, velocity uncertainty, ypr uncertainty)
-     *
-     * @param enabled Whether to enable the function
-    */
-    void enableUncertainty(bool enabled);
-
-    /** Enables status (imu status, ins status)
-     *
-     * @param enabled Whether to enable the function
-    */
-    void enableStatus(bool enabled);
-
-    /** Disables all functions
-     *
-    */
-    void disableAll();
-
-    /** Sends all register configurations to the sensor
-     *
+     * Initializes the sensor and writes registers to it
      * @returns VN::Error::None if succeeded, otherwise an error under VN::Error
     */
-    VN::Error applyRegisters();
+    VN::Error init();
 
-    /** Gets the latest data from the sensor, run once per cycle or whenever you need data
+    /** Gets the latest data from the sensor AND puts it in the state, run once per cycle or whenever you need data
      *
     */
-    void refreshData();
-
-    /** Get acceleration
-     *
-     * @returns a vector of accelerations, wrapped in an optional
-    */
-    std::optional<VN::Vec3f> getAccel() const;
-
-    /** Get angular rate
-     *
-     * @returns a vector of angular rates, wrapped in an optional
-    */
-    std::optional<VN::Vec3f> getAngularRate() const;
-
-    /** Get magnetometer data
-     *
-     * @returns a vector of magnetometer values, wrapped in an optional
-    */
-    std::optional<VN::Vec3f> getMag() const;
-
-    /** Get yaw, pitch, and roll
-     *
-     * @returns a data type with .yaw, .pitch, and .roll, wrapped in an optional
-    */
-    std::optional<VN::Ypr> getYawPitchRoll() const;
-    
-    /** Get position
-     *
-     * @returns a data type with .lat, .lon, .alt,  wrapped in an optional
-    */
-    std::optional<VN::Lla> getPosition() const;
-
-    /** Get body velocity
-     *
-     * @returns a vector of body velocities, wrapped in an optional
-    */
-    std::optional<VN::Vec3f> getVelocityBody() const;
+    void refreshDataToState(VectornavState &state);
 
 
     /** Get the next async error, if any happened
@@ -164,9 +82,6 @@ class VectorNavIMU {
 
     // Sensor and latest data
     VN::Sensor sensor{}; // IMU Sensor object
-    std::optional<VN::CompositeData> lastRawImuData; // Latest fetched raw data
-    std::optional<VN::CompositeData> lastNavData; // Latest fetched nav data
-    std::optional<VN::CompositeData> lastTimeData; // Latest fetched time data
 
     // Registers
     VN::Registers::System::BinaryOutput1 raw_imu_reg; // Raw IMU register
